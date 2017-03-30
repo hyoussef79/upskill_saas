e3e7327927de38b5d3edcd98e199abfa7a00abe1
Hesham <heshamyoussef79@gmail.com>
2017-03-30 19:41:42 +0200

    Added file field to profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,6 @@ class ProfilesController < ApplicationController
 
   private
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :phone_number, :job_title, :contact_email, :description)
+    params.require(:profile).permit(:first_name, :last_name, :avatar, :phone_number, :job_title, :contact_email, :description)
   end
 end
