2c1c8c76eb16e38d06e1d5e3bfd5a9aae5a402b3
Hesham <heshamyoussef79@gmail.com>
2017-03-31 17:40:33 +0200

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,11 @@ class ProfilesController < ApplicationController
     end
   end
 
+  # GET to /users/:user_id/profile/
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
   private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :phone_number, :job_title, :contact_email, :description)
