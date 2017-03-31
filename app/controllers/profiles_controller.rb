d56660d8a77009711a5f073322dd831ed267023d
Hesham <heshamyoussef79@gmail.com>
2017-04-01 01:48:43 +0200

    Added before action to check correct users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :only_current_user
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
@@ -41,5 +42,9 @@ class ProfilesController < ApplicationController
   private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :phone_number, :job_title, :contact_email, :description)
+  end
+  def only_current_user
+    @user = User.find( params[:user_id] )
+    redirect_to root_path unless @user == current_user
   end
 end
