ec4e376f4f3441c6d2543579e3c80f68a402ad7e
Hesham <heshamyoussef79@gmail.com>
2017-04-05 00:43:18 +0200

    Updated paperclip to upload files to Amazon S3

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ gem 'figaro', '1.1.1'
 # Use Paperclip for image upload
 gem 'paperclip', '4.3.6'
 
+gem 'aws-sdk', '< 2.0'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+# To override the default URL structure and place the bucket’s name “domain-style”
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
